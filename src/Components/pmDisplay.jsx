b8aa1d47a77e4ab58b6adfc7fd823eb4b6412ee6
Nudhana Sarutipaisan <snudhana@Nudhanas-MacBook-Pro.local>
2024-03-16 23:31:12 +0700

    Edited Tailwind

--- src/Components/pmDisplay.jsx
+++ src/Components/pmDisplay.jsx
@@ -78,78 +78,76 @@ function pmDisplay() {
     };
 
     return (
-        
-            <Box>
-                <Box sx={{
-                    padding: "2% 0 2% 2%"
-                }}>
-                    <button onClick={() => handleCategoryClick(null)} className='text-sm mx-[7px] bg-[#406695] rounded-[15px] w-[70px] text-white border-2 border-solid border-transparent hover:border-[#406695] hover:bg-white hover:text-[#406695] hover:duration-300 shadow-md'>All</button>
-                    <button onClick={() => handleCategoryClick('SIT')} className='text-sm mx-[7px] bg-[#ecbd4d] rounded-[15px] w-[70px] text-white border-2 border-solid border-transparent hover:border-[#ecbd4d] hover:bg-white hover:text-[#ecbd4d] hover:duration-300 shadow-md'>SIT</button>
-                    <button onClick={() => handleCategoryClick('CB')} className='text-sm mx-[7px] bg-[#498bb9] rounded-[15px] w-[70px] text-white border-2 border-solid border-transparent hover:border-[#498bb9] hover:bg-white hover:text-[#498bb9] hover:duration-300 shadow-md'>CB2</button>
-                    <button onClick={() => handleCategoryClick('Lx')} className='text-sm mx-[7px] bg-[#DF5935] rounded-[15px] w-[70px] text-white border-2 border-solid border-transparent hover:border-[#DF5935] hover:bg-white hover:text-[#DF5935] hover:duration-300 shadow-md'>Lx</button>
-                </Box>
-                <Box sx={{
-                    display: "flex",
-                    flexWrap: "wrap",
-                    justifyContent: "center",
-                }}>
-                    {allCards.filter((card) => selectedCategory ? card.building === selectedCategory : true).map(function (data) {
-                        return(
+        <Box>
+            <Box sx={{
+                padding: "2% 0 2% 2%"
+            }}>
+                <button onClick={() => handleCategoryClick(null)} className='text-sm mx-[7px] bg-[#406695] rounded-[15px] w-[70px] text-white border-2 border-solid border-transparent hover:border-[#406695] hover:bg-white hover:text-[#406695] hover:duration-300 shadow-md'>All</button>
+                <button onClick={() => handleCategoryClick('SIT')} className='text-sm mx-[7px] bg-[#ecbd4d] rounded-[15px] w-[70px] text-white border-2 border-solid border-transparent hover:border-[#ecbd4d] hover:bg-white hover:text-[#ecbd4d] hover:duration-300 shadow-md'>SIT</button>
+                <button onClick={() => handleCategoryClick('CB')} className='text-sm mx-[7px] bg-[#498bb9] rounded-[15px] w-[70px] text-white border-2 border-solid border-transparent hover:border-[#498bb9] hover:bg-white hover:text-[#498bb9] hover:duration-300 shadow-md'>CB2</button>
+                <button onClick={() => handleCategoryClick('Lx')} className='text-sm mx-[7px] bg-[#DF5935] rounded-[15px] w-[70px] text-white border-2 border-solid border-transparent hover:border-[#DF5935] hover:bg-white hover:text-[#DF5935] hover:duration-300 shadow-md'>Lx</button>
+            </Box>
+            <Box sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "center",
+            }}>
+                {allCards.filter((card) => selectedCategory ? card.building === selectedCategory : true).map(function (data) {
+                    return(
+                        <Box sx={{
+                            width: '450px',
+                            background: "white",
+                            boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+                            borderRadius: "20px",
+                            p: "20px",
+                            margin: "0 25px 24px",
+                            transition: ".3s",
+                            border: "2px solid transparent",
+                            cursor: 'pointer',
+                            ":hover": {
+                                borderColor: "#3f6593"
+                            }
+                        }}>
                             <Box sx={{
-                                width: '450px',
-                                background: "white",
-                                boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-                                borderRadius: "20px",
-                                p: "20px",
-                                margin: "0 25px 24px",
-                                transition: ".3s",
-                                border: "2px solid transparent",
-                                cursor: 'pointer',
-                                ":hover": {
-                                    borderColor: "#3f6593"
-                                }
+                                display: 'flex',
+                                flexDirection: 'row',
+                                justifyContent: 'flex-start'
                             }}>
+                                <img src={check(data.Pm)} className='w-[auto] h-[130px] border-[12px] border-white' alt={`Waiting for Image`}/>
                                 <Box sx={{
-                                    display: 'flex',
-                                    flexDirection: 'row',
-                                    justifyContent: 'flex-start'
-                                }}>
-                                    <img src={check(data.Pm)} className='w-[auto] h-[130px] border-[12px] border-white' alt={`Waiting for Image`}/>
-                                    <Box sx={{
-                                        pl: '30px',
+                                    pl: '30px',
 
+                                }}>
+                                    <Typography variant='h3' sx={{
+                                        fontSize: '20px',
                                     }}>
-                                        <Typography variant='h3' sx={{
-                                            fontSize: '20px',
-                                        }}>
-                                            {data.buildingRoom}
-                                        </Typography>
-                                        <Typography variant='h4' sx={{
-                                            width: '60px',
-                                            backgroundColor: buildingStyle(data.building),
-                                            borderRadius: '12px',
-                                            color: "white",
-                                            textAlign: 'center',
-                                            fontWeight: 300,
-                                            fontSize: '16px',
-                                            py: "1px"
-                                        }}>
-                                            {data.building}
-                                        </Typography>
-                                        <Typography variant='h1' sx={{
-                                            fontSize: '50px',
-                                            fontWeight: 400
-                                        }}>
-                                            {data.Pm}
-                                        </Typography>
-                                    </Box>
+                                        {data.buildingRoom}
+                                    </Typography>
+                                    <Typography variant='h4' sx={{
+                                        width: '60px',
+                                        backgroundColor: buildingStyle(data.building),
+                                        borderRadius: '12px',
+                                        color: "white",
+                                        textAlign: 'center',
+                                        fontWeight: 300,
+                                        fontSize: '16px',
+                                        py: "1px"
+                                    }}>
+                                        {data.building}
+                                    </Typography>
+                                    <Typography variant='h1' sx={{
+                                        fontSize: '50px',
+                                        fontWeight: 400
+                                    }}>
+                                        {data.Pm}
+                                    </Typography>
                                 </Box>
                             </Box>
-                        )
-                    })}
-                </Box>
+                        </Box>
+                    )
+                })}
             </Box>
-    
+        </Box>
     )
 }
 
