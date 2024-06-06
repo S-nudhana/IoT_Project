589d350f4965b70fec3f5a8b18db47654019bb2a
Nudhana Sarutipaisan <snudhana@Nudhanas-MacBook-Pro.local>
2024-06-06 10:09:45 +0700

    Finaliza

--- Iot_Backend/src/controller/getPm.js
+++ Iot_Backend/src/controller/getPm.js
@@ -1,15 +1,18 @@
 import influx from "../services/connect.js";
 
 async function getPm(req, res) {
-  const id = req.query.id;
+  const ids = req.query.id.split(','); // Allow multiple keys separated by commas
   try {
     await influx.getDatabaseNames();
-    const query = `SELECT value FROM "${id}" ORDER BY time DESC`;
-    const result = await influx.query(query);
-    if (Array.isArray(result) && result.length > 0) {
+    const promises = ids.map(id => influx.query(`SELECT value FROM "${id}" ORDER BY time DESC LIMIT 1`));
+    const results = await Promise.all(promises);
+
+    if (results.every(result => Array.isArray(result) && result.length > 0)) {
+      const values = results.map(result => result[0].value);
+      const averageValue = values.reduce((sum, value) => sum + value, 0) / values.length;
       return res.json({
         success: true,
-        data: result[0].value,
+        data: averageValue,
       });
     } else {
       return res.json({
