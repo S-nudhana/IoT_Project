e3cace114dddc3a9cf2398d1c2923f477bb36870
Nudhana Sarutipaisan <snudhana@Nudhanas-MacBook-Pro.local>
2024-06-06 09:31:09 +0700

    Added responsive to homepage

--- Iot_Frontend/src/Components/Navbar.jsx
+++ Iot_Frontend/src/Components/Navbar.jsx
@@ -5,12 +5,11 @@ import { Box } from '@mui/material';
 
 export default function Navbar() {
   return (
-    <Box className="fixed top-0 left-0 w-full z-50 bg-[#3F6593] shadow-md "> 
-      <Box className="container mx-auto px-4 py-5 flex justify-between items-center"> 
+    <Box className="fixed top-0 left-0 w-full z-50 bg-[#3F6593] shadow-md ">
+      <Box className="container mx-auto px-7 lg:px-8 xl:px-0 py-5 flex justify-start items-cesnter">
         <Link to="https://www.sit.kmutt.ac.th">
           <img src={logo} alt="" className="h-auto w-[200px]" />
         </Link>
-
       </Box>
     </Box>
   )
