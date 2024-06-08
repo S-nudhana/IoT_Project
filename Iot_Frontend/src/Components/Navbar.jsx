9bfb7188398b8f15630fd931f57b32d5d7781593
Nudhana Sarutipaisan <snudhana@Nudhanas-MacBook-Pro.local>
2024-06-08 12:22:24 +0700

    Navbar Edited

--- Iot_Frontend/src/Components/Navbar.jsx
+++ Iot_Frontend/src/Components/Navbar.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import logo from '../assets/picture/SITKMUTT_logo.png'
-import { Box } from '@mui/material';
+import { Box, AppBar, Slide, useScrollTrigger } from '@mui/material';
+
+function HideOnScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger();
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
 
 export default function Navbar() {
   return (
-    <Box className="fixed top-0 left-0 w-full z-50 bg-[#3F6593] shadow-md ">
-      <Box className="container mx-auto px-7 lg:px-8 xl:px-0 py-5 flex justify-start items-cesnter">
-        <Link to="https://www.sit.kmutt.ac.th">
-          <img src={logo} alt="" className="h-auto w-[200px]" />
-        </Link>
-      </Box>
-    </Box>
+    <HideOnScroll>
+      <AppBar sx={{
+        width: 'full',
+        backgroundColor: '#3F6593'
+      }}>
+        <Box className="container mx-auto px-7 lg:px-8 xl:px-0 py-5 flex justify-start items-cesnter">
+          <Link to="https://www.sit.kmutt.ac.th">
+            <img src={logo} alt="" className="h-auto w-[200px]" />
+          </Link>
+        </Box>
+      </AppBar>
+    </HideOnScroll>
   )
 }
