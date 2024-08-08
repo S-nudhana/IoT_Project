c03501641777ea0a699ce0cea887ab0d752a29bc
Nudhana Sarutipaisan <nu37317@gmail.com>
2024-08-08 18:06:28 +0700

    final fr

--- Iot_Backend/src/controller/getPm.js
+++ Iot_Backend/src/controller/getPm.js
@@ -19,7 +19,7 @@ async function getPm(req, res) {
         }
         return record.value;
       });
-      const averageValue = values.reduce((sum, value) => sum + value, 0) / values.length;
+      const averageValue = Math.round(values.reduce((sum, value) => sum + value, 0) / values.length);
       return res.json({
         success: true,
         data: averageValue,
