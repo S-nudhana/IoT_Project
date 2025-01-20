880edc2a07b5bd64aa46e466819e1e47e6238e78
Nudhana Sarutipaisan <nu37317@gmail.com>
2025-01-20 15:53:43 +0700

    aqi text fixed

--- Iot_Frontend/src/Components/Footer.jsx
+++ Iot_Frontend/src/Components/Footer.jsx
@@ -1,60 +1,89 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import {
-    BsFillTelephoneFill,
-    BsFillEnvelopeFill,
-    BsLine,
-    BsFacebook,
+  BsFillTelephoneFill,
+  BsFillEnvelopeFill,
+  BsLine,
+  BsFacebook,
 } from "react-icons/bs";
-import { Typography, Box } from '@mui/material';
-import logo from "../assets/picture/SIT_logo.png";
+import { Typography, Box } from "@mui/material";
+import logo from "../../public/assets/image/SIT_logo.png";
 
 export default function Footer() {
-    return (
-        <Box sx={{
-            backgroundColor: '#3F6593',
-        }}>
-            <Box sx={{
-                display: 'flex',
-                flexDirection: { xs: "column", sm: "row" },
-                alignItems: "center",
-                justifyContent: "space-evenly"
-            }}>
-                <Box>
-                    <Link to="https://www.sit.kmutt.ac.th/">
-                        <img src={logo} alt="Logo" className="w-[300px] lg:w-[500px] h-[auto]" />
-                    </Link>
-                </Box>
-                <Box sx={{
-                    color: 'whitesmoke',
-                    fontSize: { xs: "12px", md: "14px", lg: '16px' },
-                }}>
-                    <Link to="" className="hover:text-white flex pt-[28px] md:pt-[40px] lg:pt-[40px]">
-                        <BsFillTelephoneFill className='mt-[3px]' /> &nbsp;&nbsp; +66 2470 9850
-                    </Link>
-                    <Link to="mailto:webadmin@sit.kmutt.ac.th" className="hover:text-white flex pt-[10px]">
-                        <BsFillEnvelopeFill className='mt-[3px]' />&nbsp;&nbsp; webadmin@sit.kmutt.ac.th
-                    </Link>
-                    <Link to="https://www.facebook.com/SIT.Family" className="hover:text-white flex pt-[10px]">
-                        <BsFacebook className='mt-[3px]' />&nbsp;&nbsp; SIT.Family
-                    </Link>
-                    <Link to="https://page.line.me/olt5471s?openQrModal=true" className="hover:text-white flex pt-[10px] pb-[20px]">
-                        <BsLine className='mt-[3px]' />&nbsp;&nbsp; @sit.kmutt
-                    </Link>
-                </Box>
-            </Box>
-            <hr className="mt-[1%] ml-[10%] mr-[10%] bg-[#3F6593]"></hr>
-            <Typography sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                textAlign: 'center',
-                mt: '1%',
-                pb: '2%',
-                color: '#E0E5E5',
-                fontSize: { lg: "14px", md: "12px", xs: "9px" },
-                fontWeight: 400
-            }}>
-                © 2018 School of Information Technology, King Mongkut's University of Technology Thonburi.
-            </Typography>
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#3F6593",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: { xs: "column", sm: "row" },
+          alignItems: "center",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <Box>
+          <Link to="https://www.sit.kmutt.ac.th/">
+            <img
+              src={logo}
+              alt="Logo"
+              className="w-[300px] lg:w-[500px] h-[auto]"
+            />
+          </Link>
         </Box>
-    )
+        <Box
+          sx={{
+            color: "whitesmoke",
+            fontSize: { xs: "12px", md: "14px", lg: "16px" },
+          }}
+        >
+          <Link
+            to=""
+            className="hover:text-white flex pt-[28px] md:pt-[40px] lg:pt-[40px]"
+          >
+            <BsFillTelephoneFill className="mt-[3px]" /> &nbsp;&nbsp; +66 2470
+            9850
+          </Link>
+          <Link
+            to="mailto:webadmin@sit.kmutt.ac.th"
+            className="hover:text-white flex pt-[10px]"
+          >
+            <BsFillEnvelopeFill className="mt-[3px]" />
+            &nbsp;&nbsp; webadmin@sit.kmutt.ac.th
+          </Link>
+          <Link
+            to="https://www.facebook.com/SIT.Family"
+            className="hover:text-white flex pt-[10px]"
+          >
+            <BsFacebook className="mt-[3px]" />
+            &nbsp;&nbsp; SIT.Family
+          </Link>
+          <Link
+            to="https://page.line.me/olt5471s?openQrModal=true"
+            className="hover:text-white flex pt-[10px] pb-[20px]"
+          >
+            <BsLine className="mt-[3px]" />
+            &nbsp;&nbsp; @sit.kmutt
+          </Link>
+        </Box>
+      </Box>
+      <hr className="mt-[1%] ml-[10%] mr-[10%] bg-[#3F6593]"></hr>
+      <Typography
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          textAlign: "center",
+          mt: "1%",
+          pb: "2%",
+          color: "#E0E5E5",
+          fontSize: { lg: "14px", md: "12px", xs: "9px" },
+          fontWeight: 400,
+        }}
+      >
+        © 2018 School of Information Technology, King Mongkut's University of
+        Technology Thonburi.
+      </Typography>
+    </Box>
+  );
 }
--- Iot_Frontend/src/Components/Table.jsx
+++ Iot_Frontend/src/Components/Table.jsx
@@ -1,25 +1,29 @@
-import { Typography, Box } from '@mui/material'
-import table from '../assets/picture/pm.png'
+import { Typography, Box } from "@mui/material";
+import table from "../../public/assets/image/pm.png";
 
 export default function Table() {
-    return (
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            pt: "20px"
-        }}>
-            <Typography sx={{
-                textDecoration: 'underline',
-                textUnderlineOffset: "3px",
-                fontSize: { xs: "20px", md: "24px", lg: "30px" },
-                pt: '20px',
-                fontWeight: '600'
-            }}>
-                Air Quality Index (AQI) Overview
-            </Typography>
-            <img src={table} alt="" className='w-[100%] max-w-[1100px] pb-[20px]' />
-        </Box>
-    )
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        pt: "20px",
+      }}
+    >
+      <Typography
+        sx={{
+          textDecoration: "underline",
+          textUnderlineOffset: "3px",
+          fontSize: { xs: "20px", md: "24px", lg: "30px" },
+          pt: "20px",
+          fontWeight: "600",
+        }}
+      >
+        Air Quality Index (AQI) Overview
+      </Typography>
+      <img src={table} alt="" className="w-[100%] max-w-[1100px] pb-[20px]" />
+    </Box>
+  );
 }
