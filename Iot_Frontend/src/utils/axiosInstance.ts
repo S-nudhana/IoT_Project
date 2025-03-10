cb77c10ccbc69a04915127f0d9acee4e698e425e
Nudhana Sarutipaisan <79238555+S-nudhana@users.noreply.github.com>
2025-03-10 16:18:44 +0700

    Delete Iot_Frontend/src directory

--- Iot_Frontend/src/utils/axiosInstance.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_REACT_APP_API_URL,
-    timeout: 5000,
-    withCredentials: true,
-});
-
-export default axiosInstance;
