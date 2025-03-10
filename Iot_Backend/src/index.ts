7868c46ebdf2ad21c5bbfdd655c6719b2804ac1c
Nudhana Sarutipaisan <nu37317@gmail.com>
2025-03-10 20:50:29 +0700

    error fixed

--- Iot_Backend/src/index.ts
+++ Iot_Backend/src/index.ts
@@ -6,14 +6,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { corsOptions } from "./config/corsConfig";
-import { logger } from "./middleware/logger";
-
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors(corsOptions));
-app.use(logger);
 
 influx
 .getDatabaseNames()
@@ -25,7 +22,7 @@ influx
 });
 
 app.use(express.json());
-app.use("/pm", pmRouter);
+app.use("/pm", pmRouter); 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
