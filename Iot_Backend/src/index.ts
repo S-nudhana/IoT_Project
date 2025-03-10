7b7b03a7e81c77a094959eb953beafe5f5f95eb0
Nudhana Sarutipaisan <nu37317@gmail.com>
2025-03-10 20:09:14 +0700

    error fixed

--- Iot_Backend/src/index.ts
+++ Iot_Backend/src/index.ts
@@ -12,7 +12,7 @@ import { logger } from "./middleware/logger";
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(logger);
 app.use(express.json());
 
