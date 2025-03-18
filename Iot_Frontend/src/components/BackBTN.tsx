4bc50ced2ee214aed12810d260895c1c5a2f5ee4
Nudhana Sarutipaisan <nu37317@gmail.com>
2025-03-19 00:04:05 +0700

    add sensor

--- Iot_Frontend/src/components/BackBTN.tsx
+++ Iot_Frontend/src/components/BackBTN.tsx
@@ -6,7 +6,7 @@ export default function BackBTN() {
   return (
     <Link
       to="/"
-      className="flex my-[20px]"
+      className="flex my-[10px]"
     >
       <BsChevronLeft className="mt-[3px] stroke-1" />
       <Typography
